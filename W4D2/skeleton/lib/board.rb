ce98d08d6c31cc225e653da1d989e49c3d71c424
Yosyp Dobosh <joseph.dobosh@gmail.com>
2022-08-08 23:13:55 -0400

    complete initialize, place_stones, and valid_move?

--- W4D2/skeleton/lib/board.rb
+++ W4D2/skeleton/lib/board.rb
@@ -1,17 +1,27 @@
 class Board
   attr_accessor :cups
 
-  def initialize(name1, name2)
+  def initialize(name1, name2) 
+    @name1 = name1
+    @name2 = name2
+    @cups = Array.new(14) { Array.new }
+    self.place_stones
   end
 
   def place_stones
     # helper method to #initialize every non-store cup with four stones each
+    @cups.each_with_index do |cup, i|
+      4.times { |s| cup << :stone } if i != 6 && i != 13
+    end
   end
 
   def valid_move?(start_pos)
+    raise "Invalid starting cup" if !start_pos.between?(0, 11)
+    raise "Starting cup is empty" if @cups[start_pos].empty?
   end
 
   def make_move(start_pos, current_player_name)
+
   end
 
   def next_turn(ending_cup_idx)
