08989cc3f6fe45548ab3dc2cb2991bac6ef68192
Yosyp Dobosh <joseph.dobosh@gmail.com>
2022-08-11 01:19:27 -0400

    complete some specs

--- W4D4/skeleton/spec/dessert_spec.rb
+++ W4D4/skeleton/spec/dessert_spec.rb
@@ -6,34 +6,58 @@ Instructions: implement all of the pending specs (the `it` statements without bl
 =end
 
 describe Dessert do
-  let(:chef) { double("chef") }
+  subject(:dessert) { Dessert.new("cheesecake", 25, chef) }
+  let(:chef) { double("chef", :name => "chef dobosh", :titleize => "Chef Dobosh the Great Baker") }
 
   describe "#initialize" do
-    it "sets a type"
+    it "sets a type" do 
+      expect(dessert.type).to eq("cheesecake")
+    end
 
-    it "sets a quantity"
+    it "sets a quantity" do
+      expect(dessert.quantity).to eq(25)
+    end
 
-    it "starts ingredients as an empty array"
+    it "starts ingredients as an empty array" do
+      expect(dessert.ingredients).to be_empty
+    end
 
-    it "raises an argument error when given a non-integer quantity"
+    it "raises an argument error when given a non-integer quantity" do
+      expect { Dessert.new("cheesecake", "many", "Chef Dobosh") }.to raise_error(ArgumentError)
+    end
   end
 
   describe "#add_ingredient" do
-    it "adds an ingredient to the ingredients array"
+    it "adds an ingredient to the ingredients array" do
+      expect(dessert.ingredients).to_not include("egg")
+      dessert.add_ingredient("egg")
+      expect(dessert.ingredients).to include("egg")
+    end
   end
 
   describe "#mix!" do
-    it "shuffles the ingredient array"
+    let(:ingredients) { ["egg", "cream cheese", "sugar", "butter"] }
+    it "shuffles the ingredient array" do
+      dessert.mix!
+      expect(dessert.ingredients).to_not eq(ingredients)
+    end
   end
 
   describe "#eat" do
-    it "subtracts an amount from the quantity"
+    let(:quantity) { quantity = 8 }
+    it "subtracts an amount from the quantity" do
+      expect(dessert.eat(3)).to eq(5)
+    end
 
-    it "raises an error if the amount is greater than the quantity"
+    it "raises an error if the amount is greater than the quantity" do 
+      expect { dessert.eat(10) }.to raise_error("there is no more cheesecake left!")
+    end
   end
 
   describe "#serve" do
-    it "contains the titleized version of the chef's name"
+    it "contains the titleized version of the chef's name" do
+      expect (chef.serve).to include("Chef Dobosh the Great Baker")
+    end
   end
 
   describe "#make_more" do
